2cdcf1ed2e452cd9a19aa81a9fc1378ae6018801
PraneethJain <praneethjain005@gmail.com>
2023-08-26 09:25:06 +0530

    checking if statement is valid or not

--- Assignment_1/Question_2/compiler.py
+++ Assignment_1/Question_2/compiler.py
@@ -1,8 +1,6 @@
-##################### BOILERPLATE BEGINS ############################
+from rich import print
 
 
-# Token types enumeration
-##################### YOU CAN CHANGE THE ENUMERATION IF YOU WANT #######################
 class TokenType:
     IDENTIFIER = "IDENTIFIER"
     KEYWORD = "KEYWORD"
@@ -11,7 +9,9 @@ class TokenType:
     SYMBOL = "SYMBOL"
 
 
-# Token hierarchy dictionary
+Token = tuple[TokenType, str]
+operators = {"+", "-", "*", "/", "^", "<", ">", "="}
+
 token_hierarchy = {
     "if": TokenType.KEYWORD,
     "else": TokenType.KEYWORD,
@@ -19,32 +19,24 @@ token_hierarchy = {
 }
 
 
-# helper function to check if it is a valid identifier
-def is_valid_identifier(lexeme):
+def is_valid_identifier(lexeme: str) -> bool:
     if not lexeme:
         return False
 
-    # Check if the first character is an underscore or a letter
     if not (lexeme[0].isalpha() or lexeme[0] == "_"):
         return False
 
-    # Check the rest of the characters (can be letters, digits, or underscores)
-    for char in lexeme[1:]:
-        if not (char.isalnum() or char == "_"):
-            return False
-
-    return True
+    return all(char.isalnum() or char == "_" for char in lexeme[1:])
 
 
-# Tokenizer function
-def tokenize(source_code):
+def tokenize(source_code: str):
     tokens = []
     position = 0
 
     while position < len(source_code):
         # Helper function to check if a character is alphanumeric
-        def is_alphanumeric(char):
-            return char.isalpha() or char.isdigit() or (char == "_")
+        def is_alphanumeric(char: str) -> bool:
+            return char.isalnum() or (char == "_")
 
         char = source_code[position]
 
@@ -66,7 +58,6 @@ def tokenize(source_code):
             if lexeme in token_hierarchy:
                 token_type = token_hierarchy[lexeme]
             else:
-                # check if it is a valid identifier
                 if is_valid_identifier(lexeme):
                     token_type = TokenType.IDENTIFIER
                 else:
@@ -96,7 +87,8 @@ def tokenize(source_code):
                         position += 1
                     if not is_valid_identifier(lexeme):
                         raise ValueError(
-                            f"Invalid identifier: {str(lexeme)}\nIdentifier can't start with digits"
+                            f"Invalid identifier: {lexeme}\n"
+                            + "Identifier can't start with digits"
                         )
 
                 elif not next_char.isdigit():
@@ -118,13 +110,70 @@ def tokenize(source_code):
     return tokens
 
 
-########################## BOILERPLATE ENDS ###########################
+def valid_x(tokens: list[Token]) -> bool:
+    if len(tokens) == 1:
+        return tokens[0][0] in (TokenType.FLOAT, TokenType.INTEGER, TokenType.IDENTIFIER)
+    else:
+        return valid_cond(tokens)
 
 
-def checkGrammar(tokens):
-    # write the code the syntactical analysis in this function
-    # You CAN use other helper functions and create your own helper functions if needed
-    pass
+def valid_cond(tokens: list[Token]) -> bool:
+    for op in operators:
+        if (TokenType.SYMBOL, op) in tokens:
+            return valid_x(tokens[: tokens.index((TokenType.SYMBOL, op))]) and valid_x(
+                tokens[tokens.index((TokenType.SYMBOL, op)) + 1 :]
+            )
+    else:
+        return len(tokens) == 1 and tokens[0][0] in (TokenType.FLOAT, TokenType.INTEGER)
+
+
+def valid_A(tokens: list[Token]) -> bool:
+    i = 0
+    n = len(tokens)
+    i_max = -1
+    while i < n:
+        if valid_cond(tokens[:i]):
+            i_max = i
+        i += 1
+
+    if i_max == -1:
+        return False
+    
+    j = i_max + 1
+    j_max = -1
+    while j < n:
+        if valid_statement(tokens[i_max:j]):
+            j_max = j
+        j += 1
+    if j_max == -1:
+        return False
+    elif j_max == n - 1:
+        return True
+    
+    if j_max >= n or tokens[j_max] != (TokenType.KEYWORD, "else"):
+        return False
+  
+    return valid_statement(tokens[j_max+1:])
+        
+
+
+def valid_statement(tokens: list[Token]) -> bool:
+    if len(tokens) == 0:
+        return False
+    elif tokens[0] == (TokenType.KEYWORD, "if"):
+        return valid_A(tokens[1:])
+    elif len(tokens) == 1:
+        return tokens[0] not in  ((TokenType.KEYWORD, "if"), (TokenType.KEYWORD, "else"))
+    else:
+        for i in range(len(tokens)):
+            if valid_statement(tokens[:i]) and valid_statement(tokens[i:]):
+                return True
+        else:
+            return False
+
+
+def checkGrammar(tokens: list[Token]) -> bool:
+    return valid_statement(tokens)
 
 
 # Test the tokenizer
@@ -132,9 +181,7 @@ if __name__ == "__main__":
     source_code = "if 2+xi > 0 print 2.0 else print -1;"
     tokens = tokenize(source_code)
 
-    for token in tokens:
-        print(f"Token Type: {token[0]}, Token Value: {token[1]}")
+    # for token in tokens:
+    #     print(f"Token Type: {token[0]}, Token Value: {token[1]}")
 
-    logs = checkGrammar(
-        tokens
-    )  # You are tasked with implementing the function checkGrammar
+    logs = checkGrammar(tokens)
